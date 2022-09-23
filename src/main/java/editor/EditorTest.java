68d57b0c3d0ca25e72af091d043311da08b366be
hameleoncheg <111418112+hameleoncheg@users.noreply.github.com>
2022-09-23 17:39:30 +0000

    Update EditorTest.java

--- src/main/java/editor/EditorTest.java
+++ src/main/java/editor/EditorTest.java
@@ -4,12 +4,10 @@ import java.util.Calendar;
 
 public  class EditorTest {
 
-    public void namePrint(Shape s){
-        System.out.println(s.nameShape);
-    }
+
     public static void main(String[] args) {
 
-        Shape shape = new Quad("Blue","name of Shape");
+        Shape shape = new Quad("Blue","Quad");
         System.out.println(shape.getShapeType() + " is " + shape.colorShape);
 
         printShape print = new printShape();
