63b56192053c1a584102b7a7b9baa805de795e4b
hameleoncheg <hameleoncheg@gmail.com>
2022-09-26 22:26:55 +0300

    commit

--- src/main/java/MyArray/MyArrayTest.java
+++ src/main/java/MyArray/MyArrayTest.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 public class MyArrayTest {
     public static void main(String[] args) {
-        List<String> List = new ArrayList<>();
-        MyArrayList<String> List2 = new MyArrayList<>();
-        List2.add("old");
-        List2.add("old");
-        List2.add("old");
-        List2.add("old");
-        System.out.println(List2.get(2));
-        List2.add(2,"new");
-        System.out.println(List2.get(2));
+        List<String> list = new ArrayList<>();
+        MyArrayList<String> list2 = new MyArrayList<>();
+        list2.add("old");
+        list2.add("old");
+        list2.add("old");
+        list2.add("old");
+        System.out.println(list2.get(2));
+        list2.add(2,"new");
+        System.out.println(list2.get(2));
 
 
 
